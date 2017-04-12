8b53e387496878b992741f557a20d03bc2a5be06
Brendan Abolivier <contact@brendanabolivier.com>
2017-04-12 19:54:25 +0200

    Wrapper for Sequelize singleton-isation, configuration and model binding

---
+++ server/utils/sequelizeWrapper.ts
@@ -0,0 +1,56 @@
+import * as s from 'sequelize';
+
+const printit = require('printit');
+const log = printit({
+	prefix: 'Utils::SequelizeWrapper',
+	date: true
+})
+
+// Load the configuration
+const path = require('path');
+const config = require(path.resolve('.') + '/settings').database;
+
+// Load Sequelize module
+const Sequelize: s.SequelizeStatic	= require('sequelize');
+
+// Wrapper to singleton-ise Sequelize
+class SequelizeWrapper {
+	private static instance: s.Sequelize;
+	private static sync: boolean = false;
+
+	public static getInstance() {
+		if(!this.instance) {
+			let instance: s.Sequelize = new Sequelize(config.database, config.user, config.password, <s.Options>{
+				host: config.host,
+				port: config.port,
+				dialect: 'postgres',
+				define: <s.DefineOptions<any>>{
+					timestamps: false
+				}
+			});
+			
+			// Load and define Sequelize models
+			instance.define('core_config', require('../models/coreConfig'), { freezeTableName: true });
+			instance.define('user', require('../models/user'), { freezeTableName: true });
+			instance.define('plugin', require('../models/plugin'), { freezeTableName: true });
+			instance.define('plugin_data', require('../models/pluginData'), { freezeTableName: true });
+			instance.define('plugin_access', require('../models/pluginAccess'), { freezeTableName: true });
+			
+			// Create the tables if they don't already exist
+			instance.sync().then(() => {
+				log.info('Database synchronised');
+				this.sync = true;
+			});
+			
+			this.instance = instance;
+		}
+
+		return this.instance;
+	}
+
+	public static isSync() {
+		return this.sync;
+	}
+}
+
+module.exports = SequelizeWrapper;
