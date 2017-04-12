58e8ac9459d136653c1e26f2fe9e9f1fa920316f
Brendan Abolivier <contact@brendanabolivier.com>
2017-04-12 19:53:03 +0200

    Started new Americano + TypeScript project

---
+++ server.js
@@ -0,0 +1,10 @@
+var americano = require('americano');
+
+var port = process.env.PORT || 3000;
+var host = process.env.HOST || '0.0.0.0';
+americano.start({
+	name: 'Zeus',
+	root: __dirname + '/build',
+	host: host,
+	port: port
+});
