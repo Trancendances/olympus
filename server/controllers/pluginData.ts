d6c1b55320bf1ca05cbdde8a753f9043440d8154
Brendan Abolivier <contact@brendanabolivier.com>
2017-04-18 00:40:17 +0200

    Moved error handling

--- server/controllers/pluginData.ts
+++ server/controllers/pluginData.ts
@@ -1,32 +1,8 @@
 import * as p from '../lib/plugins';
 import * as e from 'express';
+import {handleErr} from '../utils/handleErrors';
 
 // TODO: Check auth headers
-
-function handleErr(err: Error, res: e.Response): void {
-	let code: number;
-
-	// Special cases
-	switch(err.message) {
-		case 'TIMESTAMP_EXISTS':
-		case 'TIMESTAMP_MISSING':
-		case 'NO_ROW_UPDATED':
-		case 'METADATA_MISMATCH':
-		case 'DATA_INVALID':
-		case 'NUMBER_MISSING':
-			code = 400;
-			break;
-		case 'IS_ADMIN':
-		case 'PLUGIN_DISABLED':
-			code = 403;
-			break;
-		default:
-			console.error(err);
-			code = 500;
-	}
-
-	res.status(code).send(err.message);
-}
 
 module.exports.get = function(req: e.Request, res: e.Response) {
 	// We must have at least one query parameter, which is the number of
---
+++ server/utils/handleErrors.ts
@@ -0,0 +1,26 @@
+import * as e from 'express';
+
+export function handleErr(err: Error, res: e.Response): void {
+	let code: number;
+
+	// Special cases
+	switch(err.message) {
+		case 'TIMESTAMP_EXISTS':
+		case 'TIMESTAMP_MISSING':
+		case 'NO_ROW_UPDATED':
+		case 'METADATA_MISMATCH':
+		case 'DATA_INVALID':
+		case 'NUMBER_MISSING':
+			code = 400;
+			break;
+		case 'NOT_ADMIN':
+		case 'PLUGIN_DISABLED':
+			code = 403;
+			break;
+		default:
+			console.error(err);
+			code = 500;
+	}
+
+	res.status(code).send(err.message);
+};
