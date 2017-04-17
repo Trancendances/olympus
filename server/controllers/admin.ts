99c87472fd19a6e84212a5b12144f0350c426725
Brendan Abolivier <contact@brendanabolivier.com>
2017-04-18 01:16:08 +0200

    Wrote last controllers for plugins

--- server/controllers/admin.ts
+++ server/controllers/admin.ts
@@ -1,6 +1,90 @@
-import {PluginConnector, State} from '../lib/plugins';
+import {PluginConnector, State, Role, AccessLevel} from '../lib/plugins';
+import {SequelizeWrapper} from '../utils/sequelizeWrapper';
+import {handleErr} from '../utils/handleErrors';
 import * as e from 'express';
+import * as s from 'sequelize';
 
-module.exports.updateState = function(req: e.Request, res: e.Response, next) {}
+const username = 'brendan'; // TEMPORARY
 
-module.exports.setAccessLevel = function(req: e.Request, res: e.Response, next) {}
+// Set home
+module.exports.home = function(req: e.Request, res: e.Response) {
+	// Action only possible for admin users
+	isAdmin(username, (err, admin) => {
+		if(err) return handleErr(err, res);
+		if(admin) {
+			// Change the home plugin according to the user input
+			PluginConnector.getInstance(req.body.plugin, (err, connector) => {
+				if(err) return handleErr(err, res);
+				// Check if connector is here, cause else TS will complain at compilation
+				if(!connector) return handleErr(new Error('CONNECTOR_MISSING'), res);
+
+				connector.setHome(req.body.disableOld, (err) => {
+					if(err) return handleErr(err, res);
+					res.sendStatus(200);
+				});
+			});
+		} else {
+			// Not admin, sorry :-(
+			return handleErr(new Error('NOT_ADMIN'), res);
+		}
+	});
+}
+
+// Change plugin state
+module.exports.changeState = function(req: e.Request, res: e.Response) {
+	// Action only possible for admin users
+	isAdmin(username, (err, admin) => {
+		if(err) return handleErr(err, res);
+		if(admin) {
+			// Change the home plugin according to the user input
+			PluginConnector.getInstance(req.body.plugin, (err, connector) => {
+				if(err) return handleErr(err, res);
+				// Check if connector is here, cause else TS will complain at compilation
+				if(!connector) return handleErr(new Error('CONNECTOR_MISSING'), res);
+
+				connector.setState(req.body.state, (err) => {
+					if(err) return handleErr(err, res);
+					res.sendStatus(200);
+				});
+			});
+		} else {
+			// Not admin, sorry :-(
+			return handleErr(new Error('NOT_ADMIN'), res);
+		}
+	});
+}
+
+// Set access level on a plugin for a given user
+module.exports.setAccessLevel = function(req: e.Request, res: e.Response) {
+	// Action only possible for admin users
+	isAdmin(username, (err, admin) => {
+		if(err) return handleErr(err, res);
+		if(admin) {
+			// Change the home plugin according to the user input
+			PluginConnector.getInstance(req.body.plugin, (err, connector) => {
+				if(err) return handleErr(err, res);
+				// Check if connector is here, cause else TS will complain at compilation
+				if(!connector) return handleErr(new Error('CONNECTOR_MISSING'), res);
+
+				let level = AccessLevel[<string>req.body.level];
+				connector.setAccessLevel(req.body.username, level, (err) => {
+					if(err) return handleErr(err, res);
+					res.sendStatus(200);
+				});
+			});
+		} else {
+			// Not admin, sorry :-(
+			return handleErr(new Error('NOT_ADMIN'), res);
+		}
+	});
+}
+
+// Check if a given user has the admin role
+export function isAdmin(username: string, next:(err: Error | null, admin?: boolean) => void) {
+	let wrapper = SequelizeWrapper.getInstance();
+	// Fetch the user then compare its role to the admin one
+	wrapper.model('user').findById(username).then((row: s.Instance<any>) => {
+		next(null, Role[<string>row.get('role')] === Role.admin)
+		return null;
+	}).catch(next);
+}
--- server/controllers/plugin.ts
+++ server/controllers/plugin.ts
@@ -1,6 +1,42 @@
 import * as p from '../lib/plugins';
 import * as e from 'express';
+import {handleErr} from '../utils/handleErrors';
+import {isAdmin} from './admin';
 
-module.exports.state = function(req, res) {}
+const username = 'brendan'; // TEMPORARY
 
-module.exports.accessLevel = function(req: e.Request, res: e.Response, next) {}
+module.exports.home = function(req: e.Request, res: e.Response, next) {
+	p.PluginConnector.getHomePluginName((err, name) => {
+		if(err) return handleErr(err, res);
+		res.status(200).send(name);
+	});
+}
+
+module.exports.state = function(req: e.Request, res: e.Response, next) {
+	p.PluginConnector.getInstance(req.params.plugin, (err, connector) => {
+		if(err) return handleErr(err, res);
+		// Check if connector is here, cause else TS will complain at compilation
+		if(!connector) return handleErr(new Error('CONNECTOR_MISSING'), res);
+		connector.getState((err, state) => {
+			if(err) return handleErr(err, res);
+			res.status(200).send(p.State[<p.State>state]);
+		});
+	});
+}
+
+module.exports.getList = function(req: e.Request, res: e.Response, next) {
+	isAdmin(username, (err, admin) => {
+		if(err) return handleErr(err, res);
+		if(admin) {
+			// If the user is admin, retrieve all plugins
+			p.PluginConnector.getPlugins(null, (err, plugins) => {
+				res.status(200).send(plugins);
+			});
+		} else {
+			// If the user isn't admin, only retrieve enabled plugins
+			p.PluginConnector.getPlugins(p.State.enabled, (err, plugins) => {
+				res.status(200).send(plugins);
+			});
+		}
+	});
+}
