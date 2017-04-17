20f98a3d200965a8cc549060efeae72d63c5f1d2
Brendan Abolivier <contact@brendanabolivier.com>
2017-04-18 01:15:11 +0200

    Added last plugin routes (yay)

--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -3,6 +3,13 @@ const plugin    = require('./plugin');
 const admin     = require('./admin');
 
 module.exports = {
+    'api/plugins': {
+        get: plugin.getList
+    },
+    'api/home': {
+        get: plugin.home,
+        post: admin.home
+    },
     'api/:plugin/data': { // Plugins' data
         get: data.get,
         post: data.add,
@@ -11,7 +18,7 @@ module.exports = {
     },
     'api/:plugin/state': { // Plugins' state
         get: plugin.state,
-        put: admin.updateState // Only admin can edit a plugin's state
+        put: admin.changeState // Only admin can edit a plugin's state
     },
     'api/:plugin/access': { // Get a user's access level to a plugin
         post: admin.setAccessLevel
