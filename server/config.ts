5a36a629f158d2302c35130d475a2e77505497df
Brendan Abolivier <contact@brendanabolivier.com>
2017-04-18 01:17:43 +0200

    The app now registers and refreshes all plugins on startup

--- server/config.ts
+++ server/config.ts
@@ -1,7 +1,8 @@
+import * as p from './lib/plugins';
+
 var americano       = require('americano'),
     fs              = require('fs'),
-    path            = require('path'),
-    pluginConnector = require('./lib/plugins').PluginConnector;
+    path            = require('path');
 
 var root = path.resolve('./plugins');
 
@@ -29,27 +30,51 @@ var config = {
     ]
 };
 
+// Check and update plugins currently stored in the database
+p.PluginConnector.update((err) => {
+    if(err) {
+        console.error(err);
+        process.exit(1);
+    }
+});
+
+// Retrieve a list of the plugins currently present in the /plugins directory
 var plugins = fs.readdirSync(root).filter(file => fs.statSync(path.join(root, file)).isDirectory());
 
 // Add plugin's public directories as static routes
 for(let plugin of plugins) {
-    // Get (unused) instance of the connector so the plugin gets registered if
+    // Get instance of the connector so the plugin gets registered if
     // it wasn't before
-    pluginConnector.getInstance(plugin, (err) => {
+    p.PluginConnector.getInstance(plugin, (err, connector) => {
         if(err) return console.error(err);
+        if(!connector) {
+            console.error(new Error('Couldn\'t get database connector'));
+            return process.exit(1);
+        }
+
+        connector.getState((err, state) => {
+            if(err) {
+                console.error(err);
+                return process.exit(1);
+            }
+            // Only create the root if the plugin is enabled
+            if(state === p.State.enabled) {
+                // Get the plugin's public path
+                let pluginPublicPath = path.join(root, plugin, 'public');
+                
+                // Check if the plugin has a public directory
+                if(fs.existsSync(pluginPublicPath)) {
+                    // Create a static route for the plugin
+                    config.common.use.push([
+                        '/' + plugin,
+                        americano.static(pluginPublicPath, {
+                            maxAge: 86400000
+                        })
+                    ]);
+                }
+            }
+        });
     });
-    // Get the plugin's public path
-    let pluginPublicPath = path.join(root, plugin, 'public');
-    // Check if the plugin has a public directory
-    if(fs.existsSync(pluginPublicPath)) {
-        // Create a static route for the plugin
-        config.common.use.push([
-            '/' + plugin,
-            americano.static(pluginPublicPath, {
-                maxAge: 86400000
-            })
-        ]);
-    }
 }
 
 module.exports = config;
